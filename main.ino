38054c6dc766ff06e0f5bd7e4f232d1520269b25
AugustinEnx <a.esnoux@gmail.com>
2018-04-09 18:24:10 +0200

    Fini Intero

--- main.ino
+++ main.ino
@@ -6,11 +6,14 @@
 // the setup function runs once when you press reset or power the board
 void setup()
 {
+    pinMode (13, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
-    static int potPosition;
-    potPosition = analogRead (A1); 
+    digitalWrite (13,HIGH);
+    delay (4000);
+    digitalWrite(13, LOW);
+    delay(4000);
 }
